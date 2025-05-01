9b013dfe74575a560eadb7782e04d88487336107
MojammelSojib <151757406+MojammelSojib@users.noreply.github.com>
2025-05-02 01:06:26 +0600

    Add files via upload

---
+++ pattern_tringale_star.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            cout<<"*";
+            
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
